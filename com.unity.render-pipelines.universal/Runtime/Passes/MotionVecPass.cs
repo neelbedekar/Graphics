46376dde9e023db470023f108f8f908169c41181
Neel Bedekar <neel.bedekar@gmail.com>
2021-03-01 15:30:22 -0800

    Update MotionVecPass to remove unnecessary code
    Cleaning up a bit, removing code that's not needed.

--- com.unity.render-pipelines.universal/Runtime/Passes/MotionVecPass.cs
+++ com.unity.render-pipelines.universal/Runtime/Passes/MotionVecPass.cs
@@ -22,27 +22,6 @@ namespace UnityEngine.Rendering.Universal.Internal
         RenderTargetIdentifier motionVectorColorIdentifier;
         RenderTargetIdentifier motionVectorDepthIdentifier;
 
-        static Material s_ErrorMaterial2;
-        static Material errorMaterial2
-        {
-            get
-            {
-                if (s_ErrorMaterial2 == null)
-                {
-                    // TODO: When importing project, AssetPreviewUpdater::CreatePreviewForAsset will be called multiple times.
-                    // This might be in a point that some resources required for the pipeline are not finished importing yet.
-                    // Proper fix is to add a fence on asset import.
-                    try
-                    {
-                        s_ErrorMaterial2 = new Material(Shader.Find("Hidden/Universal Render Pipeline/DepthOnlyMotionVec"));
-                    }
-                    catch { }
-                }
-
-                return s_ErrorMaterial2;
-            }
-        }
-
         static readonly int s_DrawObjectPassDataPropID = Shader.PropertyToID("_DrawObjectPassData");
 
         public MotionVecPass(string profilerTag, ShaderTagId[] shaderTagIds, bool opaque, RenderPassEvent evt, RenderQueueRange renderQueueRange, LayerMask layerMask, StencilState stencilState, int stencilReference)
@@ -103,41 +82,11 @@ namespace UnityEngine.Rendering.Universal.Internal
             CommandBuffer cmd = CommandBufferPool.Get();
             using (new ProfilingScope(cmd, m_ProfilingSampler))
             {
-                // Global render pass data containing various settings.
-                // x,y,z are currently unused
-                // w is used for knowing whether the object is opaque(1) or alpha blended(0)
-                Vector4 drawObjectPassData = new Vector4(0.0f, 0.0f, 0.0f, (m_IsOpaque) ? 1.0f : 0.0f);
-                cmd.SetGlobalVector(s_DrawObjectPassDataPropID, drawObjectPassData);
-
-                // scaleBias.x = flipSign
-                // scaleBias.y = scale
-                // scaleBias.z = bias
-                // scaleBias.w = unused
-                float flipSign = (renderingData.cameraData.IsCameraProjectionMatrixFlipped()) ? -1.0f : 1.0f;
-                Vector4 scaleBias = (flipSign < 0.0f)
-                    ? new Vector4(flipSign, 1.0f, -1.0f, 1.0f)
-                    : new Vector4(flipSign, 0.0f, 1.0f, 1.0f);
-                cmd.SetGlobalVector(ShaderPropertyId.scaleBiasRt, scaleBias);
-
                 context.ExecuteCommandBuffer(cmd);
                 cmd.Clear();
 
                 Camera camera = renderingData.cameraData.camera;
-                var sortFlags = (m_IsOpaque) ? renderingData.cameraData.defaultOpaqueSortFlags : SortingCriteria.CommonTransparent;
-                //var drawSettings = CreateDrawingSettings(m_ShaderTagIdList, ref renderingData, sortFlags);
-                //drawSettings.overrideMaterial = errorMaterial2;
-                //drawSettings.overrideMaterialPassIndex = 0;
                 var filterSettings = m_FilteringSettings;
-
-                #if UNITY_EDITOR
-                // When rendering the preview camera, we want the layer mask to be forced to Everything
-                if (renderingData.cameraData.isPreviewCamera)
-                {
-                    filterSettings.layerMask = -1;
-                }
-                #endif
-
-                //context.DrawRenderers(renderingData.cullResults, ref drawSettings, ref filterSettings, ref m_RenderStateBlock);
 
                 // Render objects that did not match any shader pass with error shader
                 RenderingUtils.RenderObjectsWithError(context, ref renderingData.cullResults, camera, filterSettings, SortingCriteria.None, true, m_ShaderTagIdList, PerObjectData.MotionVectors);
